fb32b5b2c3f23b3aa6138ce65f149378621c2970
shi <958885266@qq.com>
2019-01-07 16:18:23 +0800

    第2次尝试修改

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("呵呵  马上ok");
+        System.out.println("第二次修改");
     }
 }
